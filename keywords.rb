eb341a16fff3ccd8cfe4feb8e65070977bf18a91
Joey Kunin <Blackhole@JoeMBP-3.local>
2014-09-12 09:16:03 -0700

    added hashtag and raw output functions for insta and tumblr

--- keywords.rb
+++ keywords.rb
@@ -3,10 +3,29 @@ require 'xmp'
 require 'exifr'
 require 'open-uri'
 
-Dir.glob('*.jpg') do |jpg_file|
-  puts "#{jpg_file.to_s}"
-  img = EXIFR::JPEG.new(jpg_file)
-  xmp = XMP.parse(img)
-  hashtags = xmp.lr.hierarchicalSubject.collect {|x| "#" + x.chomp}
-  puts hashtags.join(" ")
+def hashtags
+  puts "Instagram".upcase
+  Dir.glob('*.jpg') do |jpg_file|
+    puts "#{jpg_file.to_s.upcase}"
+    img = EXIFR::JPEG.new(jpg_file)
+    xmp = XMP.parse(img)
+    hashtags = xmp.lr.hierarchicalSubject.collect {|x| "#" + x.chomp}
+    puts hashtags.join(" ")
+    puts "\n\n"
+  end
 end
+
+def keywords
+  puts "Tumblr/Keywords".upcase
+  Dir.glob('*.jpg') do |jpg_file|
+    puts "#{jpg_file.to_s.upcase}"
+    img = EXIFR::JPEG.new(jpg_file)
+    xmp = XMP.parse(img)
+    hashtags = xmp.lr.hierarchicalSubject
+    puts hashtags.join(" ")
+    puts "\n\n"
+  end
+end
+
+hashtags
+keywords
