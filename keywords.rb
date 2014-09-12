0bda6fed85ac40b2b2d301081370d87652585cf6
Joey Kunin <Blackhole@JoeMBP-3.local>
2014-09-12 14:04:40 -0700

    added upload to flickr capability, with recursive tagging and captions

--- keywords.rb
+++ keywords.rb
@@ -1,9 +1,10 @@
-# gem install xmp exifr
+# gem install xmp exifr open-uri tumblr_client
 require 'xmp'
 require 'exifr'
 require 'open-uri'
 require 'tumblr_client'
 
+#authenticate tumblr plugin
 Tumblr.configure do |config|
   config.consumer_key = "ytWN3IBf2xB6pyR12eycbyoh0QzOM8gpQZHgj9mzRlW3KlvQhS"
   config.consumer_secret = "E1ym8E7hNePPoL5vpGdAoieSYOKRZwbrH2z210YUY6wm5KZ8oz"
@@ -11,42 +12,16 @@ Tumblr.configure do |config|
   config.oauth_token_secret = "HPj1ssG7z3VPWTJeCiMweqTZzmsT09xdzPjNeldh3Mfc1QcLrp"
 end
 
+#new tumblr client instance
+  client = Tumblr::Client.new
+  
+  Dir.glob('*.jpg') do |jpg_file|
+      img = EXIFR::JPEG.new(jpg_file)
+      xmp = XMP.parse(img)
+      tags = xmp.lr.hierarchicalSubject.join(",")
+      client.photo('joekuninphoto.tumblr.com', :state => "queue", :caption => "www.joekuninphoto.com", :link =>"", :data => "#{jpg_file}", :tags => "#{tags}", )
+  end
 
-client = Tumblr::Client.new
-
-#uploads photo - :data must be array of filenames, :tags only comma seperated
-
-client.photo('joekuninphoto.tumblr.com', :data => ["1.jpg"], :tags => "more tasty tags, or,only,with,commas", :state => "queue")
-
-
-
-
-
-
-# Tagging functions
-#def hashtags
-#  puts "Instagram".upcase
-#  Dir.glob('*.jpg') do |jpg_file|
-#    puts "#{jpg_file.to_s.upcase}"
-#    img = EXIFR::JPEG.new(jpg_file)
-#    xmp = XMP.parse(img)
-#    hashtags = xmp.lr.hierarchicalSubject.collect {|x| "#" + x.chomp}
-#    puts hashtags.join(" ")
-#    puts "\n\n"
-#  end
-#end
-#
-#def keywords
-#  puts "Tumblr/Keywords".upcase
-#  Dir.glob('*.jpg') do |jpg_file|
-#    puts "#{jpg_file.to_s.upcase}"
-#    img = EXIFR::JPEG.new(jpg_file)
-#    xmp = XMP.parse(img)
-#    hashtags = xmp.lr.hierarchicalSubject
-#    puts hashtags.join(" ")
-#    puts "\n\n"
-#  end
-#end
-#
-#hashtags
-#keywords
+  #NOTES: currently uploads directory of images to tumblr queue with tags pulled from lightroom metadata (try to use single level tags) 
+  #TODO: 1) make executable from server. 2) attach to chron job 3) check for unique images 4) repaste duplicates that are updated from LR (using timestamp?)
+  
